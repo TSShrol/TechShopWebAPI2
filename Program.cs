babe6f535333df331ea64db36f837c59a28613b0
TSShrol <shrolts@gmail.com>
2022-12-10 18:19:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using BAL.Services;
+using BAL.Interface;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TechShopDbContext>(options=>options.UseInMemoryDatabase("TechShopDb"));
+
+
+//add custom services
+builder.Services.AddScoped<IProductService,IProductService>();
+//builder.Services.AddScoped
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
